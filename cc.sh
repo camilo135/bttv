b955bc2151b8e291597059acfa1ce186ac2a9a7c
camilo135 <47012970+camilo135@users.noreply.github.com>
2023-03-01 21:15:47 +0100

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 nix-shell -p mplayer
-wget https://github.com/camilo135/bttv/blob/main/bttv.mp4
+wget https://raw.githubusercontent.com/camilo135/bttv/main/bttv.mp4
 export CACA_DRIVER=ncurses
-cp bttv.mp4 ../
-rm -r ../bttv
-while true; do mplayer -really-quiet -vo caca ../bttv.mp4; done
+while true; do mplayer -really-quiet -vo caca bttv.mp4; done
 
 
